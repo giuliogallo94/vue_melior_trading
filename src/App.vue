721953d7f9ce3c2fd8e5c8b6aceaa0d8ce7f9071
giuliogallo94 <giuliogallo1994@gmail.com>
2024-09-26 15:49:59 +0200

    add: create Navbar component

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,19 @@
 <script>
+import AppNavbar from "./components/AppNavbar.vue";
 export default {
-  components: {},
+  components: { AppNavbar },
 };
 </script>
 
-<template></template>
+<template>
+  <AppNavbar />
+</template>
 
-<style lang="scss"></style>
+<style lang="scss">
+body {
+  width: 100vw;
+  height: 100vh;
+  background-color: black;
+  color: azure;
+}
+</style>
---
+++ src/components/AppNavbar.vue
@@ -0,0 +1,12 @@
+<script></script>
+
+<template>
+  <nav>Melior Trading</nav>
+</template>
+
+<style lang="scss" scoped>
+nav {
+  width: 80%;
+  background-color: blue;
+}
+</style>
