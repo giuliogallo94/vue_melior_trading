514416c84a718cb9e23813b77872a6d1c1d8f7cc
giuliogallo94 <giuliogallo1994@gmail.com>
2024-09-27 13:17:38 +0200

    add: links' array in store

--- src/data/store.js
+++ src/data/store.js
@@ -1,3 +1,36 @@
 import { reactive } from "vue";
 
-export const store = reactive({});
+export const store = reactive({
+  sideBarLinks: [
+    {
+      name: "home",
+      path: "/",
+      spanText: "home",
+    },
+    {
+      name: "dashboard",
+      path: "/dashboard",
+      spanText: "apps",
+    },
+    {
+      name: "tradesList",
+      path: "/tradesList",
+      spanText: "format_list_bulleted",
+    },
+    {
+      name: "moreStats",
+      path: "/moreStats",
+      spanText: "monitoring",
+    },
+    {
+      name: "pnl",
+      path: "/pnl",
+      spanText: "donut_large",
+    },
+    {
+      name: "traderProfile",
+      path: "/traderProfile",
+      spanText: "person",
+    },
+  ],
+});
